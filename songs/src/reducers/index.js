9614f5a238ce3e2c9b9751b004fd310def8b5fee
Said Robley <saidrobley@gmail.com>
2019-02-19 19:11:01 -0700

    finalize the project

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,15 +1,16 @@
 import { combineReducers } from 'redux';
+
 const songsReducer = () => {
   return [
-    { title: 'No Scrubs', duaration: '4:05' },
+    { title: 'No Scrubs', duration: '4:05' },
     { title: 'Macarena', duaration: '2:30' },
-    { title: 'All Star', duaration: '3:15' },
-    { title: 'I Want it That Way', duaration: '1:45' }
+    { title: 'All Star', duration: '3:15' },
+    { title: 'I Want it That Way', duration: '1:45' }
   ];
 };
 
 const selectedSongReducer = (selectedSong = null, action) => {
-  if (action.type == 'SONG_SELECTED') {
+  if (action.type === 'SONG_SELECTED') {
     return action.payload;
   }
   return selectedSong;
